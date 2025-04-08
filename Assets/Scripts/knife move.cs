e5152d896f9df38f4d160a63deca8dcbf66c6af6
Sam Seid <samanthaseid11@gmail.com>
2025-04-08 15:10:35 -0400

    work on assignment

--- Assets/Scripts/knife move.cs
+++ Assets/Scripts/knife move.cs
@@ -7,31 +7,14 @@ using UnityEngine.Events;
 public class knifemove : MonoBehaviour
 {
     
-    public Transform leftRest;
-    //left restriction for knife
-    public Transform rightRest;
-    //right restriction for knife
-    public UnityEvent<int> tooClose;
-    // alerts that you're too close to the knife
-    public AudioSource audiosource;
-    public AudioClip clip;
-    //use sound
+   
 
     float knifeMoveLeft;
-    float knifeMoveRight;
 
     Coroutine knifeSwinging;
-    Coroutine swingingKnife;
     IEnumerator swing;
-    IEnumerator swing2;
+   
     //swing
-
-    //public AnimationCurve ani;
-    public BoxCollider2D knife;
-  
-
-    float oneSwing;
-
     public float speed = 0.5f;
     //speed of knife
     public float p = 0;
@@ -44,16 +27,6 @@ public class knifemove : MonoBehaviour
         knifeSwinging = StartCoroutine(MoveKnife());
         //start movement
     }
-
-    private void Update()
-    {
-        if (movement == false)
-        {
-            swingingKnife = StartCoroutine(MoveKnife2());
-        }
-    }
-
-
     IEnumerator MoveKnife()
     {
         while (true)
@@ -65,16 +38,6 @@ public class knifemove : MonoBehaviour
     }
 
 
-    IEnumerator MoveKnife2()
-    {
-        while (true)
-        {
-            swing2 = MoveRight();
-            yield return StartCoroutine(swing2);
-        }
-    }
-
-
     IEnumerator MoveLeft()
     {
         if (p < speed && movement == true)
@@ -82,7 +45,6 @@ public class knifemove : MonoBehaviour
 
             p += Time.deltaTime;
             transform.Rotate(0, 0, (20 / speed) * Time.deltaTime);
-            // rotate.Rotate = Vector2.Lerp(leftRest.position, rightRest.position, ani.Evaluate(p));
         }
         else { 
             //move clockwise
@@ -90,30 +52,14 @@ public class knifemove : MonoBehaviour
             yield return null;
             movement = false;
         }
-        //if (audiosource.isPlaying == false)
-        //{
-        //    audiosource.PlayOneShot(clip);
-        //}
+        
     }
 
-
-    IEnumerator MoveRight()
-    {
-        while (p < speed && movement==false)
-        {
-            p += Time.deltaTime;
-            transform.Rotate(0, 0, (50 / speed) * Time.deltaTime);
-            yield return null;
-            //move counter clockwise
-        }
-    }
 
     public void StopKnifeMovement()
     {
         StopCoroutine(knifeSwinging);
         StopCoroutine(swing);
-        StopCoroutine(swingingKnife);
-        StopCoroutine(swing2);
     }
    
 }
--- Assets/Scripts/player movement.cs
+++ Assets/Scripts/player movement.cs
@@ -2,6 +2,7 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class playermovement : MonoBehaviour
 {
@@ -10,16 +11,24 @@ public class playermovement : MonoBehaviour
     //access animator 
     SpriteRenderer spri;
     // access player sprite renderer
+
     public float speed = 2;
     //speed of jump
+    public float j;
+
     public bool Jump = true;
     //if player is able to jump
-    public bool attack = false;
 
-    public float j;
-    public AnimationCurve curve;
-    public Transform top;
-    public Transform bottom;
+
+    public int attack = 0;
+    //if attack add to streak
+    public UnityEvent<int> KillStreak;
+    //kill streak screen
+
+    public AudioSource audiosource;
+    public AudioClip clip;
+    //use sound
+
 
 
 
@@ -37,28 +46,41 @@ public class playermovement : MonoBehaviour
     void Update()
     {
 
-        float upward = Input.GetAxis("Vertical");
+        //float upward = Input.GetAxis("Vertical");
         //go vertical
-        ani.SetFloat("jump up", Mathf.Abs(upward));
+        //ani.SetFloat("jump up", Mathf.Abs(upward));
 
         if (Input.GetMouseButtonDown(0))
         {
             ani.SetTrigger("attack");
             //when left mouse is clicked, set trigger to do attack animation
             Jump = false;
-            attack = true;
-            //cannot jump while attacking
+            //don't allow jump
+            attack++;
+            //add to streak
+            Debug.Log("Your damaging the enemy!");
+        }
+
+        if(attack > 4)
+        {
+            Debug.Log("Kill Streak!");
+            //kill streak is working
+            KillStreak.Invoke(attack);
+            //make all things in event happen
+            
+            
+            audiosource.PlayOneShot(clip);
+            // play sound when player kills enemy
         }
 
 
-       
-        if(Jump == true)
+        if (Jump == true)
         {
            if (Input.GetKeyDown(KeyCode.Space))
             {
                 ani.SetTrigger("jump");
                 Jump = true;
-                attack = false;
+                
                 //transform.position += transform.up * upward * speed * Time.deltaTime;
                 //transform.position = Vector2.Lerp(bottom.position, top.position, curve.Evaluate(j));
                 Debug.Log("pressed space");
@@ -71,7 +93,9 @@ public class playermovement : MonoBehaviour
     public void GotHurt()
     {
         ani.SetTrigger("hurt");
+        //got hurt
         Jump = false;
+        //don't jump while hurt
     }
 
    
